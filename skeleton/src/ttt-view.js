8182ec1b2c7851cd6f6b4f6ae88bc4bdf5988a32
Chris Mann <chrismann809@gmail.com>
2021-02-24 15:15:50 -0500

    wrote bindEvents, makeMove, and setupBoard for ttt-view, added styling

--- skeleton/src/ttt-view.js
+++ skeleton/src/ttt-view.js
@@ -2,26 +2,37 @@ class View {
   constructor(game, $el) {
     this.game = game;
     this.$el = $el;
+    this.setupBoard();
+    this.bindEvents();
   }
 
-  bindEvents() {}
+  bindEvents() {
+    this.$el.on('click', "li", (e) => {
+      let $li = $(e.currentTarget);
+      this.game.playMove($li.data('pos'));
+      $li.text(this.game.currentPlayer);
+      this.makeMove($li);
+      // console.log(this.game.currentPlayer);
+    });
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
+    $square.addClass('filled');
+  }
 
-  setupBoard() {}
-}
-
-View.prototype.setupBoard = function() {
-  const $rows = $('<ul>');
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      let $col = $('<li>');
-      $col.data('pos', [i, j]);
-      $rows.append($col);
+  setupBoard() {
+    const $rows = $('<ul>');
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let $col = $('<li>');
+        $col.data('pos', [i, j]);
+        $rows.append($col);
+      }
     }
-  }
 
-  this.$el.append($rows);
+    this.$el.append($rows);
+  }
 }
+
 
 module.exports = View;
